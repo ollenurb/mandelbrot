3ac67cd076788150ff842e5b3f6a5fbbae46b121
Ollenurb <brunello.mtt@gmail.com>
2021-04-02 03:19:20 +0200

    Added parallel computation support using the Repa library

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Mandelbrot Fractal
-Simple program that displays the mandelbrot fractal.
+A simple program that generates an image of the mandelbrot fractal. It uses
+[repa](https://hackage.haskell.org/package/repa) and [JuicyPixels](https://hackage.haskell.org/package/JuicyPixels). 
+In order to exploit parallelism provided by the repa package, you need to have `llvm` installed. 
 
-To run just use `stack build` and `stack run`. 
+### Running the program
+The project uses stack as a build tool, run `stack build` to compile, then `stack run -- +RTS -s
+-N4` to run the program using 4 processing cores. The program will then generate an image file 
+`mandelbrot.png` in the root directory.
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,16 @@
+{-# LANGUAGE TypeOperators   #-}
+
 module Main where
 
 import Data.Complex
 import Data.Word8
 import Codec.Picture
+import qualified Data.Array.Repa as R
+import Data.Array.Repa (Array, DIM1, DIM2, U, D, Z (..), (:.)(..), (!))
+
+
+-- Type that represents a single Pixel that will be used by Repa
+type RGB8 = (Pixel8, Pixel8, Pixel8)
 
 maxIterations :: Int
 maxIterations = 255
@@ -13,6 +21,9 @@ imgWidth = 1920
 imgHeight :: Int
 imgHeight = 1080
 
+imgPath :: String
+imgPath = "./mandel.png"
+
 f :: RealFloat a => Complex a -> Complex a -> Complex a
 f c z = z^2 + c
 
@@ -20,18 +31,30 @@ fractal :: RealFloat a => Complex a -> Complex a -> Int
 fractal c init = length . takeWhile (\x -> magnitude x <= 2) . take maxIterations $ iterated
     where iterated = iterate (f c) init
 
+scaleInterval :: Fractional a => (a, a) -> (a, a) -> a -> a
+scaleInterval (a, b) (c, d) t = c + (((d - c) * (t - a)) / (b - a))
+
 main :: IO ()
-main = writePng "./mandel.png" mandelbrotImage
+main = do
+    putStrLn $ "Writing mandelbrot set in " ++ imgPath ++ ", please wait."
+    repaVectorImage <- R.computeUnboxedP generateImgRepa
+    let mandelbrotImage = repaArrayToImage repaVectorImage
+    writePng imgPath mandelbrotImage
+    putStrLn "Image successfully written."
 
-mandelbrotImage :: Image PixelRGB8
-mandelbrotImage = generateImage generatePixel imgWidth imgHeight
-
-generatePixel :: Int -> Int -> PixelRGB8
-generatePixel x y = PixelRGB8 iters iters iters
+generatePixel :: (Z :. Int :. Int) -> RGB8
+generatePixel (Z :. x :. y) = (iters, iters, iters)
     where
         iters = fromIntegral $ fractal (scX :+ scY) (0 :+ 0)
         scY = scaleInterval (0.0, fromIntegral imgHeight) (-1.0, 1.0) $ fromIntegral y
         scX = scaleInterval (0.0, fromIntegral imgWidth) (-2.0, 0.5) $ fromIntegral x
 
-scaleInterval :: Fractional a => (a, a) -> (a, a) -> a -> a
-scaleInterval (a, b) (c, d) t = c + (((d - c) * (t - a)) / (b - a))
+generateImgRepa :: Array D DIM2 RGB8
+generateImgRepa = R.fromFunction (Z :. imgWidth :. imgHeight) generatePixel
+
+repaArrayToImage :: Array U DIM2 RGB8 -> Image PixelRGB8
+repaArrayToImage a = generateImage gen imgWidth imgHeight
+  where
+    gen x y =
+      let (r, g, b) = a ! (Z :. x :. y)
+      in PixelRGB8 r g b
